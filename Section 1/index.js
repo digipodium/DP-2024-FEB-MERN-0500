96e59d6882b120ba9562e62d9dc39cac41744b99
m-triple-m <triplem656@gmail.com>
2024-02-17 18:40:09 +0530

    javascript

---
+++ Section 1/index.js
@@ -0,0 +1,31 @@
+console.log('Welcome to JavaScript');
+
+console.log('nice');
+
+num = 45;
+
+myname = 'Mubassir';
+
+console.log(num);
+
+console.log(myname);
+
+console.log( typeof num );
+
+price = 999.99
+
+console.log( typeof price );
+
+console.log(parseInt(price));
+
+console.log( 5 + 1 + '1' );
+
+console.log( true + '1' );
+
+console.log( 1 == '1' );
+console.log( 1 === '1' );
+
+// exponent operator
+
+console.log( 5 ** 2 );
+console.log( 4 ** 5 );
---
+++ Section 1/keywords.js
@@ -0,0 +1,16 @@
+if(68 % 2 === 0){
+    console.log('even');
+
+    num1 = 3674;
+
+    let num2 = 3874;
+    const pi = 3.1416;
+    num2 = pi + 3456;
+    console.log(num2);
+
+}else{
+    console.log('odd');
+}
+
+console.log(num1);
+console.log(num2);
