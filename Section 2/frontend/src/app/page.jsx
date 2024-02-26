3ccf7786057963fd803b31ea1088b9a97284cfb6
m-triple-m <triplem656@gmail.com>
2024-02-26 09:56:51 +0530

    react setup

---
+++ Section 2/frontend/src/app/page.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+      <h1>My Next.js App</h1>
+      <img src="https://upload.wikimedia.org/wikipedia/en/thumb/3/3b/SpongeBob_SquarePants_character.svg/1200px-SpongeBob_SquarePants_character.svg.png" alt="" />
+      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cupiditate excepturi maiores possimus labore. Veritatis necessitatibus accusantium eum illum molestiae explicabo.</p>
+    </div>
+  )
+}
+
+export default Home
