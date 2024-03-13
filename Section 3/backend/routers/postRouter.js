cee64257b53a7f076fdb46a7684179934b74ac65
m-triple-m <triplem656@gmail.com>
2024-03-13 18:37:09 +0530

    add operation complete

--- Section 3/backend/routers/postRouter.js
+++ Section 3/backend/routers/postRouter.js
@@ -1,9 +1,19 @@
 const express = require('express');
 const router = express.Router();
+const Model = require('../models/postModel');
 
 router.post('/add', (req, res) => {
     console.log(req.body);
-    res.send('post add operation');
+
+    // to save data in mongodb
+    new Model(req.body).save()
+    .then((result) => {
+        res.status(200).json(result);
+    }).catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+
 });
 
 router.get('/getall', (req, res) => {
