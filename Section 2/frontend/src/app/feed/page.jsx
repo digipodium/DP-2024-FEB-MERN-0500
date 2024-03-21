968fd7712e81c1a560ca78005977ddfe0a62d19c
m-triple-m <triplem656@gmail.com>
2024-03-21 17:25:50 +0530

    fetch data

--- Section 2/frontend/src/app/feed/page.jsx
+++ Section 2/frontend/src/app/feed/page.jsx
@@ -1,14 +1,23 @@
 'use client';
-import React, { useEffect } from 'react'
+import { IconHeart } from '@tabler/icons-react';
+import React, { useEffect, useState } from 'react'
 
 const Feed = () => {
+
+    const [postArray, setPostArray] = useState([]);
 
     const fetchPostData = () => {
 
         fetch('http://localhost:5000/post/getall')
         .then((response) => {
             console.log(response.status);
-        }).catch((err) => {
+            return response.json();
+        })
+        .then((data) => {
+            console.log(data);
+            setPostArray(data);
+        })
+        .catch((err) => {
             console.log(err);
         });
     }
@@ -22,6 +31,26 @@ const Feed = () => {
         <div className='container py-4'>
             <h1 className='text-center'>Trending Posts</h1>
             <hr />
+
+            <div className='col-md-6 mx-auto'>
+                {
+                    postArray.map( (post) => {
+                        return <div key={post._id} className='card shadow mb-5' >
+                            <div className='card-header'>
+                                <h4>{post.title}</h4>
+                            </div>
+                            <img className='card-img-top' src={post.image} alt="" />
+                            <div className='card-footer'>
+                                <div className='d-flex g-4'>
+                                    <button className='btn btn-outline-primary w-100'>{post.likes} <IconHeart/> </button>
+                                    <button className='btn btn-outline-primary w-100'>{post.shares} Share</button>
+                                </div>
+                            </div>
+                        </div>
+                    })
+                }
+            </div>
+
         </div>
 
     </div>
