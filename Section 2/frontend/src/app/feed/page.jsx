917db8b43181c33f77fd5c691ee74f2c61432b7c
m-triple-m <triplem656@gmail.com>
2024-03-21 18:04:42 +0530

    delete operation

--- Section 2/frontend/src/app/feed/page.jsx
+++ Section 2/frontend/src/app/feed/page.jsx
@@ -1,5 +1,5 @@
 'use client';
-import { IconHeart } from '@tabler/icons-react';
+import { IconHeart, IconTrash } from '@tabler/icons-react';
 import React, { useEffect, useState } from 'react'
 
 const Feed = () => {
@@ -9,52 +9,70 @@ const Feed = () => {
     const fetchPostData = () => {
 
         fetch('http://localhost:5000/post/getall')
-        .then((response) => {
-            console.log(response.status);
-            return response.json();
-        })
-        .then((data) => {
-            console.log(data);
-            setPostArray(data);
-        })
-        .catch((err) => {
-            console.log(err);
-        });
+            .then((response) => {
+                console.log(response.status);
+                return response.json();
+            })
+            .then((data) => {
+                console.log(data);
+                setPostArray(data);
+            })
+            .catch((err) => {
+                console.log(err);
+            });
     }
 
     useEffect(() => {
-      fetchPostData();
+        fetchPostData();
     }, []);
 
-  return (
-    <div>
-        <div className='container py-4'>
-            <h1 className='text-center'>Trending Posts</h1>
-            <hr />
+    const deletePost = (id) => {
+        fetch('http://localhost:5000/post/delete/' + id, {
+            method: 'DELETE'
+        })
+            .then((response) => {
+                if (response.status === 200) {
+                    console.log('post deleted');
+                    fetchPostData();
+                }
+            })
+            .catch((err) => {
+                console.log(err);
+            });
+    }
 
-            <div className='col-md-6 mx-auto'>
-                {
-                    postArray.map( (post) => {
-                        return <div key={post._id} className='card shadow mb-5' >
-                            <div className='card-header'>
-                                <h4>{post.title}</h4>
-                            </div>
-                            <img className='card-img-top' src={post.image} alt="" />
-                            <div className='card-footer'>
-                                <div className='d-flex g-4'>
-                                    <button className='btn btn-outline-primary w-100'>{post.likes} <IconHeart/> </button>
-                                    <button className='btn btn-outline-primary w-100'>{post.shares} Share</button>
+    return (
+        <div>
+            <div className='container py-4'>
+                <h1 className='text-center'>Trending Posts</h1>
+                <hr />
+
+                <div className='col-md-6 mx-auto'>
+                    {
+                        postArray.map((post) => {
+                            return <div key={post._id} className='card shadow mb-5' >
+                                <div className='card-header d-flex justify-content-between'>
+                                    <h4>{post.title}</h4>
+                                    <button className='btn btn-danger' onClick={() => { deletePost(post._id) }} >
+                                        <IconTrash />
+                                    </button>
+                                </div>
+                                <img className='card-img-top' src={post.image} alt="" />
+                                <div className='card-footer'>
+                                    <div className='d-flex g-4'>
+                                        <button className='btn btn-outline-primary w-100'>{post.likes} <IconHeart /> </button>
+                                        <button className='btn btn-outline-primary w-100'>{post.shares} Share</button>
+                                    </div>
                                 </div>
                             </div>
-                        </div>
-                    })
-                }
+                        })
+                    }
+                </div>
+
             </div>
 
         </div>
-
-    </div>
-  )
+    )
 }
 
 export default Feed;
--- Section 3/backend/routers/postRouter.js
+++ Section 3/backend/routers/postRouter.js
@@ -27,4 +27,15 @@ router.get('/getall', (req, res) => {
     });
 });
 
+// : denotes url parameter
+router.delete('/delete/:id', (req, res) => {
+    Model.findByIdAndDelete(req.params.id)
+    .then((result) => {
+        res.status(200).json(result);
+    }).catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+})
+
 module.exports = router;
